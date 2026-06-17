5f719c26a67f442fd85c0d0b22a43d32e394a419
KamyarKazemi <151082088+KamyarKazemi@users.noreply.github.com>
2026-06-17 12:03:10 +0330

    done

--- src/redux/thunks/sendOtpLogin.tsx
+++ src/redux/thunks/sendOtpLogin.tsx
@@ -1,20 +1,26 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const URL = import.meta.env.VITE_BACKEND_URL_LOGIN_SECOND;
+const OTP_URL = import.meta.env.VITE_BACKEND_URL_LOGIN_SECOND;
+const ACCESS_URL = import.meta.env.VITE_BACKEND_URL_ACCESS;
 
 interface VerifyPayload {
   otp_code: string;
   otp_session_token: string;
 }
 
+interface UserProfile {
+  id: number;
+  username?: string;
+  phone?: string;
+  email?: string;
+}
+
 interface VerifyOtpResponse {
   access_token: string;
   refresh_token: string;
+  user: UserProfile;
 }
-
-console.log("ENV:", import.meta.env);
-console.log("SECOND URL:", URL);
 
 export const sendOtpLoginThunk = createAsyncThunk<
   VerifyOtpResponse,
@@ -22,25 +28,34 @@ export const sendOtpLoginThunk = createAsyncThunk<
   { rejectValue: string }
 >("otp/send", async ({ otp_code, otp_session_token }, { rejectWithValue }) => {
   try {
-    const response = await axios.post(URL, {
+    const response = await axios.post(OTP_URL, {
       otp_code,
       otp_session_token,
     });
 
-    console.log("sendOtpLoginThunk dispatched:", {
-      otp_code,
+    const accessToken = response.data.access;
+    const refreshToken = response.data.refresh;
+
+    localStorage.setItem("access_token", accessToken);
+    localStorage.setItem("refresh_token", refreshToken);
+
+    const profileResponse = await axios.get(ACCESS_URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
 
-    console.log("ACCESS TOKEN:", response.data.access);
-    console.log("REFRESH TOKEN:", response.data.refresh);
+    console.log("USER INFO FROM BACKEND:", profileResponse.data);
 
     return {
-      access_token: response.data.access,
-      refresh_token: response.data.refresh,
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      user: profileResponse.data,
     };
   } catch (error: any) {
     return rejectWithValue(
       error?.response?.data?.message ||
+        error?.response?.data?.detail ||
         error?.response?.data ||
         "تأیید کد ناموفق بود",
     );
