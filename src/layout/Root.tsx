3352ccd70d582c7fbd13687b47d7372c9a5152f7
KamyarKazemi <151082088+KamyarKazemi@users.noreply.github.com>
2026-06-29 00:03:21 +0330

    nothing

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -6,11 +6,7 @@ export default function Root() {
   return (
     <div dir="rtl" className="min-h-screen flex flex-col">
       <Header />
-
-      {/* <main className="flex-1"> */}
       <Outlet />
-      {/* </main> */}
-
       <Footer />
     </div>
   );
